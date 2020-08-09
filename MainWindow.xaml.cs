7a6a1ba4e4b1dadba0be3a98bafa7e6b9421d40e
FreakLign <stlign@outlook.com>
2020-08-09 11:51:40 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DayPlan
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataBinding dataBinding = new DataBinding();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = dataBinding;
+            
+        }
+        public void AddNewPlanItem(string PlanHeader, string TimeAtPlan, string PlanInfo)
+        {
+            PlanQueen.Children.Add(new PlanItem(PlanHeader, PlanInfo, TimeAtPlan));
+        }
+        public class DataBinding : INotifyPropertyChanged
+        {
+            private string _backgroundPictureSource = @"/pic1.jpg";
+            public string BackGoundPicture
+            {
+                get { return _backgroundPictureSource; }
+                set
+                {
+                    _backgroundPictureSource = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs("BackGoundPicture"));
+                    }
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewPlanItem("Item", DateTime.Now.ToString(), "详细阐述日行的功能");
+        }
+    }
+}
---
+++ PlanItem.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DayPlan
+{
+    /// <summary>
+    /// PlanItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class PlanItem : UserControl
+    {
+        PlanItemContent _content = new PlanItemContent();
+        public PlanItem()
+        {
+            InitializeComponent();
+            this.DataContext = _content;
+        }
+        public PlanItem(string HeadText, string MoreInfo, string Date)
+        {
+            InitializeComponent();
+            this.DataContext = _content;
+            _content.PlanItemHeader = HeadText;
+            _content.MoreInfo = MoreInfo;
+            _content.PlanTime = Date;
+        }
+        public class PlanItemContent : INotifyPropertyChanged
+        {
+            private string _planItemContent;
+            private string _planTime;
+            private string _moreInfo;
+            public string PlanItemHeader
+            {
+                get { return _planItemContent; }
+                set
+                {
+                    _planItemContent = value;
+                    if (PropertyChanged != null) PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PlanItemContent"));
+                }
+            }
+            public string PlanTime
+            {
+                get { return _planTime; }
+                set
+                {
+                    _planTime = value;
+                    if (PropertyChanged != null) PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PlanTime"));
+                }
+            }
+            public string MoreInfo
+            {
+                get { return _moreInfo; }
+                set
+                {
+                    _moreInfo = value;
+                    if (PropertyChanged != null) PropertyChanged.Invoke(this, new PropertyChangedEventArgs("MoreInfo"));
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+    }
+}
